5164c000421ab8353e01ac2c1a354016577034e3
PIjakipour <p.ijakipour@gmail.com>
2017-04-13 11:25:16 +0100

    Update and rename train_3.ino to train_3_PIedit.ino
    Added comments.

--- train_3_PIedit.ino
+++ train_3_PIedit.ino
@@ -1,8 +1,8 @@
 // Simple train monitoring system 
 // By Spencer Organ
 // Visit home.uktechreviews.com for more projects and blog
-
-
+//PI for Qasim: int is short for integer and it defines a variable which will later be used in the code. If you don't define a variable your code will not work.
+//All the variable names are pretty self explanetory. Great job by SO. 
 int track1_in = 0;         // variable for reading the pushbutton status for entering section 1
 int track1_out = 0;        // variable for reading the pushbutton status for leaving section 1
 int track2_in = 0;         // variable for reading the pushbutton status for entering section 2
@@ -10,16 +10,21 @@ int track2_out = 0;       // variable for reading the pushbutton status for leav
 int track3_in = 0;         // variable for reading the pushbutton status for entering section 3
 int track3_out = 0;       // variable for reading the pushbutton status for leaving section 3
 
+//For these 3 variables they need to be set to false, meaning all 3 lights will be green at first, unless there's a train in that section
+
 int section1 = 0;         // train in section 1 initally set to false (0)
 int section2 = 0;         // train in section 2 initally set to false (0)
 int section3 = 0;         // train in section 2 initally set to false (0)
 
 int error =0;
-
+//Here's where the code starts
+//pinMode is a funcion of Arduino. You use it to configure the pins either to INPUT or OUTPUT. So A0, A1, A5 are the pins and you've just set them to act as outputs. 
 void setup() {
   pinMode(A0, OUTPUT);
   pinMode(A1, OUTPUT);
   pinMode(A5, OUTPUT);
+  //INPUT_PULLUP is a function that enables the built-in resistor to make the pin more responsive and practically drown out the background noise. 
+  //Watch this video to understand it better https://www.youtube.com/watch?v=wxjerCHCEMg
   pinMode(2, INPUT_PULLUP);
   pinMode(3, INPUT_PULLUP);
   pinMode(4, OUTPUT);
@@ -34,8 +39,9 @@ void setup() {
 
 test_leds();      // Call the function to test all the LEDs and the buzzer
 open_track();     // Call the function which resets the track
-
+//Serial begin sets the data rate in bits per second (baud) for serial data transmission
 Serial.begin(9600);  
+  //You need to use printIn to actually tell the Ardiuno to output something.
   Serial.println("--- Start Serial Monitor SEND_RCVE ---");
   Serial.println();
 
@@ -52,8 +58,10 @@ void loop() {
   track2_out = digitalRead(7);
   track3_in = digitalRead(10);
   track3_out = digitalRead(11);
-
+//Here we are using an if funcion. It's saying that if there's a voltage in track 1, i.e. the botton at the begining of track one has been pushed
+//set section1 to no longer be "false" but have a value, which means there's a train in section 1. 
   if (track1_in != HIGH) {
+    //You're also definding how A1 and A0 pins should react.
   section1=1; digitalWrite(A1,HIGH);digitalWrite(A0,LOW);
   Serial.println("Train just came into section1");
     } 
@@ -61,10 +69,11 @@ void loop() {
     if (track1_out != HIGH){
     section1=0;digitalWrite(A0,HIGH);digitalWrite(A1,LOW);
     Serial.println("Train just left section1");
+      //Here's is plan B. You shut down the power if there's a train in section 2 and train in section one is just about to leave. 
     if (section2==1){alarm();error=1;}
     else {error=0;}
     }
-
+//Same story for the other 2 sections. 
    
     if (track2_in != HIGH) {
     section2=1; digitalWrite(5,HIGH);digitalWrite(4,LOW);
@@ -98,7 +107,7 @@ Serial.println(section3);
 check_status();
 
 }
-
+//This is when you're testing the LEDs. The 200 delay is a 0.2 seconds delay for each LED. You don't need to mention this part in the report though.
 int test_leds(){
 digitalWrite(A0, HIGH);
 delay(200);
@@ -134,7 +143,7 @@ delay(200);
 digitalWrite(A5, LOW);
 delay(200);
 }
-
+//Here you are providing voltage to the track. 
 int open_track(){
   digitalWrite(A0, HIGH);
   digitalWrite(4, HIGH);
